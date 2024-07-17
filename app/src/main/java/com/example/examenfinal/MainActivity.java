f12eb6c00ca936c29ec31711f7d18aca61e0f102
Axel <20b3010455@untels.edu.pe>
2024-07-17 10:02:18 -0500

    Login

---
+++ app/src/main/java/com/example/examenfinal/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.examenfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
